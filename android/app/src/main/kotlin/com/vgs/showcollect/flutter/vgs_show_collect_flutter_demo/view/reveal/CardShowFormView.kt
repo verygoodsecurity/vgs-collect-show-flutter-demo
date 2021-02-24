def555cbe131f5dd3f436d99943d80f8aebd8311
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-02-24 13:48:17 +0200

    Initial Commit

---
+++ android/app/src/main/kotlin/com/vgs/showcollect/flutter/vgs_show_collect_flutter_demo/view/reveal/CardShowFormView.kt
@@ -0,0 +1,43 @@
+package com.vgs.showcollect.flutter.vgs_show_collect_flutter_demo.view.reveal
+
+import android.content.Context
+import android.util.Log
+import com.verygoodsecurity.vgsshow.VGSShow
+import com.verygoodsecurity.vgsshow.core.network.client.VGSHttpMethod
+import com.vgs.showcollect.flutter.vgs_show_collect_flutter_demo.MainActivity
+import com.vgs.showcollect.flutter.vgs_show_collect_flutter_demo.R
+import com.vgs.showcollect.flutter.vgs_show_collect_flutter_demo.view.BaseFormView
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+
+class CardShowFormView constructor(context: Context, messenger: BinaryMessenger?, id: Int) :
+        BaseFormView(context, messenger, id, R.layout.show_form_layout) {
+
+    override val viewType: String get() = MainActivity.SHOW_FORM_VIEW_TYPE
+
+    private val vgsShow = VGSShow(context, MainActivity.TENANT_ID, MainActivity.ENVIRONMENT)
+
+    init {
+
+        vgsShow.subscribe(rootView.findViewById(R.id.tvCardNumber))
+        vgsShow.subscribe(rootView.findViewById(R.id.tvCardExpiration))
+    }
+
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        when (call.method) {
+            "revealCard" -> revealCard(call, result)
+        }
+    }
+
+    private fun revealCard(call: MethodCall, result: MethodChannel.Result) {
+        val data = call.arguments as ArrayList<*>
+        runOnBackground {
+            Log.d("Test", call.arguments.toString())
+            vgsShow.request("post", VGSHttpMethod.POST, mapOf(
+                    "payment_card_number" to data[0],
+                    "payment_card_expiration_date" to data[1]
+            ))
+        }
+    }
+}
---
+++ lib/main.dart
@@ -0,0 +1,227 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+typedef void CardCollectFormCallback(CardCollectFormController controller);
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+const COLLECT_FORM_VIEW_TYPE = 'card-collect-form-view';
+const SHOW_FORM_VIEW_TYPE = 'card-show-form-view';
+
+const CARD_TOKEN_KEY = 'cardNumber';
+const DATE_TOKEN_KEY = 'expDate';
+
+class _MyHomePageState extends State<MyHomePage> {
+  CardCollectFormController collectController;
+  CardShowFormController showController;
+
+  String cardToken;
+  String dateToken;
+
+  @override
+  Widget build(BuildContext context) {
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          backgroundColor: Color(0xff3c4c5d),
+        ),
+        body: Row(children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 12.0, top: 12.0, right: 6.0, bottom: 12.0),
+              child: Column(children: <Widget>[
+                Expanded(
+                  child: _cardCollect(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 12.0),
+                  child: _collectButton(),
+                ),
+                Text("Powered by VGS Collect SDK",
+                    style:
+                        TextStyle(fontSize: 10.0, fontWeight: FontWeight.bold)),
+              ]),
+            ),
+          ),
+          VerticalDivider(
+            color: Color(0xff3c4c5d),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 6.0, top: 16.0, right: 12.0, bottom: 12.0),
+              child: Column(children: <Widget>[
+                Expanded(
+                  child: _cardShow(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 12.0),
+                  child: _showButton(),
+                ),
+                Text("Powered by VGS Show SDK",
+                    style:
+                        TextStyle(fontSize: 10.0, fontWeight: FontWeight.bold)),
+              ]),
+            ),
+          ),
+        ]),
+      );
+    }
+    return Center(
+        child: Text("Not supported",
+            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)));
+  }
+
+  Widget _cardCollect() {
+    final Map<String, dynamic> creationParams = <String, dynamic>{};
+    return PlatformViewLink(
+      viewType: COLLECT_FORM_VIEW_TYPE,
+      surfaceFactory:
+          (BuildContext context, PlatformViewController controller) {
+        return AndroidViewSurface(
+          controller: controller,
+          gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
+          hitTestBehavior: PlatformViewHitTestBehavior.opaque,
+        );
+      },
+      onCreatePlatformView: (PlatformViewCreationParams params) {
+        var platformView = PlatformViewsService.initSurfaceAndroidView(
+          id: params.id,
+          viewType: COLLECT_FORM_VIEW_TYPE,
+          layoutDirection: TextDirection.ltr,
+          creationParams: creationParams,
+          creationParamsCodec: StandardMessageCodec(),
+        );
+        platformView
+            .addOnPlatformViewCreatedListener(params.onPlatformViewCreated);
+        platformView
+            .addOnPlatformViewCreatedListener(_createCardCollectController);
+        platformView.create();
+        return platformView;
+      },
+    );
+  }
+
+  Widget _collectButton() {
+    return MaterialButton(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+      color: Color(0xff3c4c5d),
+      child: new Text('Collect',
+          style: new TextStyle(fontSize: 16.0, color: Colors.white)),
+      onPressed: () {
+        collectController.redactCardAsync().then((value) {
+          cardToken = value.entries
+              .firstWhere((element) => element.key == CARD_TOKEN_KEY)
+              .value;
+          dateToken = value.entries
+              .firstWhere((element) => element.key == DATE_TOKEN_KEY)
+              .value;
+        });
+      },
+    );
+  }
+
+  void _createCardCollectController(int id) {
+    print("View id = $id");
+    collectController = new CardCollectFormController._(id);
+  }
+
+  Widget _cardShow() {
+    final Map<String, dynamic> creationParams = <String, dynamic>{};
+    return PlatformViewLink(
+      viewType: SHOW_FORM_VIEW_TYPE,
+      surfaceFactory:
+          (BuildContext context, PlatformViewController controller) {
+        return AndroidViewSurface(
+          controller: controller,
+          gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
+          hitTestBehavior: PlatformViewHitTestBehavior.opaque,
+        );
+      },
+      onCreatePlatformView: (PlatformViewCreationParams params) {
+        var platformView = PlatformViewsService.initSurfaceAndroidView(
+          id: params.id,
+          viewType: SHOW_FORM_VIEW_TYPE,
+          layoutDirection: TextDirection.ltr,
+          creationParams: creationParams,
+          creationParamsCodec: StandardMessageCodec(),
+        );
+        platformView
+            .addOnPlatformViewCreatedListener(params.onPlatformViewCreated);
+        platformView
+            .addOnPlatformViewCreatedListener(_createCardShowController);
+        platformView.create();
+        return platformView;
+      },
+    );
+  }
+
+  Widget _showButton() {
+    return MaterialButton(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+      color: Color(0xff3c4c5d),
+      child: new Text('Show',
+          style: new TextStyle(fontSize: 16.0, color: Colors.white)),
+      onPressed: () {
+        showController.revealCardAsync(cardToken, dateToken);
+      },
+    );
+  }
+
+  void _createCardShowController(int id) {
+    print("View id = $id");
+    showController = new CardShowFormController._(id);
+  }
+}
+
+class CardCollectFormController {
+  CardCollectFormController._(int id)
+      : _channel = new MethodChannel('$COLLECT_FORM_VIEW_TYPE/$id');
+
+  final MethodChannel _channel;
+
+  Future<Map<dynamic, dynamic>> redactCardAsync() async {
+    return await _channel.invokeMethod('redactCard', null);
+  }
+}
+
+class CardShowFormController {
+  CardShowFormController._(int id)
+      : _channel = new MethodChannel('$SHOW_FORM_VIEW_TYPE/$id');
+
+  final MethodChannel _channel;
+
+  Future<void> revealCardAsync(String cardToken, String dateToken) async {
+    return await _channel.invokeMethod('revealCard', [cardToken, dateToken]);
+  }
+}
