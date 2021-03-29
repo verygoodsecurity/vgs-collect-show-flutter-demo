f0c28a10aecc0d6e8841535a7e45e4730efb6bf3
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-03-29 13:23:03 +0300

    Update text placeholder

--- ios/Runner/DemoAppConfig.swift
+++ ios/Runner/DemoAppConfig.swift
@@ -9,7 +9,7 @@ final class DemoAppConfig {
 
 	// MARK: - Vars
 
-  var vaultId = "VGS_TEST_VAULT_ID"
+  var vaultId = "<YOUR_VAULT_ID>"
   var path = "post"
 	let payloadKey = "VGS_TEST_PAYLOAD_KEY"
 	let payloadValue = "VGS_TEST_PAYLOAD_VALUE"
