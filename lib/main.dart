824983eaac8976a9f59e16e881d9063138725990
EugeneIOs <72962856+EugeneIOs@users.noreply.github.com>
2021-03-30 12:47:57 +0300

    Flutter iOS Sample

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,13 @@ const SHOW_FORM_VIEW_TYPE = 'card-show-form-view';
 const CARD_TOKEN_KEY = 'cardNumber';
 const DATE_TOKEN_KEY = 'expDate';
 
+const COLLECT_ERROR_CODE_KEY = 'collect_error_code';
+const COLLECT_ERROR_MESSAGE_KEY = 'collect_error_message';
+
+const SHOW_SUCCESS_CODE_KEY = 'show_status_code';
+const SHOW_ERROR_CODE_KEY = 'show_error_code';
+const SHOW_ERROR_MESSAGE_KEY = 'show_error_message';
+
 class _MyHomePageState extends State<MyHomePage> {
   CardCollectFormController collectController;
   CardShowFormController showController;
@@ -48,58 +55,64 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    if (defaultTargetPlatform == TargetPlatform.android) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          backgroundColor: Color(0xff3c4c5d),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Color(0xff3c4c5d),
+      ),
+      body: Row(children: <Widget>[
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(
+                left: 12.0, top: 12.0, right: 6.0, bottom: 12.0),
+            child: Column(children: <Widget>[
+              Expanded(child: Builder(builder: (context) {
+                // any logic needed...
+                final isAndorid =
+                    defaultTargetPlatform == TargetPlatform.android;
+
+                return isAndorid ? _cardCollect() : _cardCollectNativeiOS();
+              })),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 12.0),
+                child: _collectButton(),
+              ),
+              Text("Powered by VGS Collect SDK",
+                  style:
+                      TextStyle(fontSize: 10.0, fontWeight: FontWeight.bold)),
+            ]),
+          ),
         ),
-        body: Row(children: <Widget>[
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(
-                  left: 12.0, top: 12.0, right: 6.0, bottom: 12.0),
-              child: Column(children: <Widget>[
-                Expanded(
-                  child: _cardCollect(),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 12.0),
-                  child: _collectButton(),
-                ),
-                Text("Powered by VGS Collect SDK",
-                    style:
-                        TextStyle(fontSize: 10.0, fontWeight: FontWeight.bold)),
-              ]),
-            ),
+        VerticalDivider(
+          color: Color(0xff3c4c5d),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(
+                left: 6.0, top: 16.0, right: 12.0, bottom: 12.0),
+            child: Column(children: <Widget>[
+              Expanded(child: Builder(builder: (context) {
+                // any logic needed...
+                final isAndorid =
+                    defaultTargetPlatform == TargetPlatform.android;
+
+                return isAndorid ? _cardShow() : _cardShowNativeiOS();
+              })),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 12.0),
+                child: _showButton(),
+              ),
+              Text("Powered by VGS Show SDK",
+                  style:
+                      TextStyle(fontSize: 10.0, fontWeight: FontWeight.bold)),
+            ]),
           ),
-          VerticalDivider(
-            color: Color(0xff3c4c5d),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(
-                  left: 6.0, top: 16.0, right: 12.0, bottom: 12.0),
-              child: Column(children: <Widget>[
-                Expanded(
-                  child: _cardShow(),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 12.0),
-                  child: _showButton(),
-                ),
-                Text("Powered by VGS Show SDK",
-                    style:
-                        TextStyle(fontSize: 10.0, fontWeight: FontWeight.bold)),
-              ]),
-            ),
-          ),
-        ]),
-      );
-    }
-    return Center(
-        child: Text("iOS is not implemented, will be available soon.",
-            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)));
+        ),
+      ]),
+    );
+    // return Center(
+    //     child: Text("iOS is not implemented, will be available soon.",
+    //         style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)));
   }
 
   Widget _cardCollect() {
@@ -140,12 +153,30 @@ class _MyHomePageState extends State<MyHomePage> {
           style: new TextStyle(fontSize: 16.0, color: Colors.white)),
       onPressed: () {
         collectController.redactCardAsync().then((value) {
+          final entries = value.entries;
+          print("value.entries: $entries");
+
+          final errorCode = value[COLLECT_ERROR_CODE_KEY];
+          if (errorCode != null) {
+            print("error!");
+            print("VGS Collect SDK error code: $errorCode");
+
+            final errorMessage = value[COLLECT_ERROR_MESSAGE_KEY];
+            if  (errorMessage != null) {
+               print("VGS Collect SDK error message: $errorMessage");
+            }
+            return;
+          }
+
           cardToken = value.entries
               .firstWhere((element) => element.key == CARD_TOKEN_KEY)
               .value;
           dateToken = value.entries
               .firstWhere((element) => element.key == DATE_TOKEN_KEY)
               .value;
+
+          print("cardToken from collect: $cardToken");
+          print("dateToken from collect: $dateToken");
         });
       },
     );
@@ -186,6 +217,32 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  Widget _cardCollectNativeiOS() {
+    final Map<String, dynamic> creationParams = <String, dynamic>{};
+    return Column(children: [
+      SizedBox(
+          height: 136.0,
+          child: UiKitView(
+              viewType: COLLECT_FORM_VIEW_TYPE,
+              onPlatformViewCreated: _createCardCollectController,
+              creationParams: creationParams,
+              creationParamsCodec: StandardMessageCodec()))
+    ]);
+  }
+
+  Widget _cardShowNativeiOS() {
+    final Map<String, dynamic> creationParams = <String, dynamic>{};
+    return Column(children: [
+      SizedBox(
+          height: 136.0,
+          child: UiKitView(
+              viewType: SHOW_FORM_VIEW_TYPE,
+              onPlatformViewCreated: _createCardShowController,
+              creationParams: creationParams,
+              creationParamsCodec: StandardMessageCodec()))
+    ]);
+  }
+
   Widget _showButton() {
     return MaterialButton(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
@@ -193,13 +250,33 @@ class _MyHomePageState extends State<MyHomePage> {
       child: new Text('Reveal',
           style: new TextStyle(fontSize: 16.0, color: Colors.white)),
       onPressed: () {
-        showController.revealCardAsync(cardToken, dateToken);
+        showController.revealCardAsync(cardToken, dateToken).then((value) {
+          final entries = value.entries;
+          print("show value.entries: $entries");
+
+          final errorCode = value[SHOW_ERROR_CODE_KEY];
+          if (errorCode != null) {
+            print("error!");
+            print("VGS Show SDK error code: $errorCode");
+
+            final errorMessage = value[SHOW_ERROR_MESSAGE_KEY];
+            if  (errorMessage != null) {
+               print("VGS Show SDK error message: $errorMessage");
+            }
+            return;
+          }
+
+          final successStatusCode = value[SHOW_SUCCESS_CODE_KEY];
+          if (successStatusCode != null) {
+              print("VGS Show success status code: $successStatusCode");
+          }
+        });
       },
     );
   }
 
   void _createCardShowController(int id) {
-    print("View id = $id");
+    print("Show View id = $id");
     showController = new CardShowFormController._(id);
   }
 }
@@ -221,7 +298,7 @@ class CardShowFormController {
 
   final MethodChannel _channel;
 
-  Future<void> revealCardAsync(String cardToken, String dateToken) async {
+  Future <Map<dynamic, dynamic>> revealCardAsync(String cardToken, String dateToken) async {
     return await _channel.invokeMethod('revealCard', [cardToken, dateToken]);
   }
 }
